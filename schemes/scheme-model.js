96812ec05592aa5c7a04caa4ff67d394ea107482
Erik Scow <erikscow@gmail.com>
2020-07-30 18:13:12 -0700

    Added db-config and started schema-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,24 @@
+const db = require('../db-config')
+
+function find() {
+}
+
+function findById(id) {
+}
+
+function add(user) {
+}
+
+function update(changes, id) {
+}
+
+function remove(id) {
+}
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+}

5796265a34006197e46de9c233b84635a8f1ae9e
Erik Scow <erikscow@gmail.com>
2020-07-30 18:49:04 -0700

    Wrote first draft of schema model db helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,18 +1,40 @@
 const db = require('../db-config')
 
 function find() {
+   return db('schemas')
 }
 
 function findById(id) {
+    if (db('schemas').where({ id })) {
+        return db('schemas').where({ id }).first()
+    } else {
+        return null
+    }
+    
 }
 
 function add(user) {
+    db('schema').insert(user)
+        .then(ids => {
+            return findById(ids[0])
+        })
 }
 
 function update(changes, id) {
+    db('schema').where({ id }).update(changes)
+        .then(ids => {
+            return findById(ids[0])
+        })
 }
 
 function remove(id) {
+
+    db('schema').findById(id)
+        .then(schema => {
+            const deleted = schema
+        })
+    db('schema').where({ id }).del()
+    return deleted
 }
 
 module.exports = {
